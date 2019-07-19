49015479d8d12d41b5f5de4cff30899e7474c2e6
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-07-19 18:09:40 -0400

    Cleaned up

--- BeagleBone/AI/pru/resource_table_0.h
+++ BeagleBone/AI/pru/resource_table_0.h
@@ -57,8 +57,9 @@
 #define HOST_UNUSED		255
 
 /* Mapping sysevts to a channel. Each pair contains a sysevt, channel. */
-struct ch_map pru_intc_map[] = { {18, 2},
-				 {19, 0},
+struct ch_map pru_intc_map[] = { 
+	{18, 2},
+	{19, 0},
 };
 
 struct my_resource_table {
