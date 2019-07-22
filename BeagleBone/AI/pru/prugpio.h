b21fbe2f0b78c4ba73b9666db7ef737a531270f9
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-07-22 11:27:26 -0400

    Added Black R30 pins

--- BeagleBone/AI/pru/prugpio.h
+++ BeagleBone/AI/pru/prugpio.h
@@ -55,6 +55,16 @@
 #define USR2 (1<<23)
 #define USR3 (1<<24)
 
+// R30 output bits on pru0
+#define P9_31   (1<<0)
+#define P9_29   (1<<1)
+#define P9_30   (1<<2)
+#define P9_28   (1<<3)
+#define P9_92   (1<<4)
+#define P9_27   (1<<5)
+#define P9_91   (1<<6)
+#define P9_25   (1<<7)
+
 // Shared memory
 #define AM33XX_DATARAM0_PHYS_BASE		0x4a300000
 #define AM33XX_DATARAM1_PHYS_BASE		0x4a302000
