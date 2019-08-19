e69add145d04d3865bb299a49191f26419221f83
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-08-19 13:31:54 -0400

    Switched to error-first callback

--- BeagleBone/Black/bone101/potentiometer.js
+++ BeagleBone/Black/bone101/potentiometer.js
@@ -2,7 +2,7 @@
 var b = require('bonescript');
 b.analogRead('P9_40', printAIN1);
 
-function printAIN1(x) {
-    console.log('x.value = ' + x.value);
-    console.log('x.err = ' + x.err);
+function printAIN1(err, value) {
+    console.log('value = ' + value);
+    console.log('err = ' + err);
 }                    
