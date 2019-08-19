4069238ba6b0dc478ab787a619e78ab48f3c70bf
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-08-19 13:28:40 -0400

    Added correct code

--- BeagleBone/Black/bone101/potentiometer.js
+++ BeagleBone/Black/bone101/potentiometer.js
@@ -1,19 +1,8 @@
 #!/usr/bin/env node
 var b = require('bonescript');
-var analogVoltage = 0;
+b.analogRead('P9_40', printAIN1);
 
-/* Check the sensor values every 2 seconds*/
-setInterval(read, 2000);
-
-function read(){
-    b.analogRead('P9_40', printStatus);
-}
-
-function printStatus(x) {
-    var distanceInches;
-    analogVoltage = x.value*1.8; // ADC Value converted to voltage
-    console.log('x.value = ' + analogVoltage); 
-    distanceInches = analogVoltage / 0.00699;
-    console.log("There is an object " + 
-    parseFloat(distanceInches).toFixed(3) + " inches away.");
-}
+function printAIN1(x) {
+    console.log('x.value = ' + x.value);
+    console.log('x.err = ' + x.err);
+}                    
