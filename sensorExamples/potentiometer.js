6d0f9a0ded10fb01828d55857b8b878dccfded54
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-08-21 13:39:47 -0400

    Switched to A5 and added table

--- sensorExamples/potentiometer.js
+++ sensorExamples/potentiometer.js
@@ -5,13 +5,27 @@ var b = require('bonescript');
 // -------------- | ---  | ----- | ------
 // outer terminal | 1.8V | P9_32 | P1_18
 // outer terminal | GND  | P9_34 | P1_17
-// wiper (middle) | A1   | P9_40 | P1_21
+// wiper (middle) | A5   | P9_46 | P1_21
 
-b.analogRead('A1', printAIN1);
+function start() {
+    b.analogRead('A5', printA);
+}
 
-function printAIN1(err, value) {
+setInterval(start, 500);
+
+function printA(err, value) {
     console.log('value = ' + value);
     if(err) {
         console.log('err = ' + err);
     }
 }                    
+
+// Bone  | Pocket | AIN
+// ----- | ------ | --- 
+// P9_39 | P1_19  | A0
+// P9_40 | P1_21  | A1
+// P9_37 | P1_23  | A2
+// P9_38 | P1_25  | A3
+// P9_33 | P1_27  | A4
+// P9_36 | P2_35  | A5
+// P9_35 | P1_02  | A6
