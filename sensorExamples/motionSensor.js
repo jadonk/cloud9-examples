ab4d4ad4a6f885dedbf037cf8d323b91a9f83716
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-08-21 14:13:01 -0400

    Added wiring diagram to motion sensor

--- sensorExamples/motionSensor.js
+++ sensorExamples/motionSensor.js
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
 var b = require('bonescript');
-var led = "P8_13";
-b.pinMode(led, 'out');
-b.pinMode('P8_19', b.INPUT);
+var LED = "P9_14";
+var AL  = "P8_19";
+
+b.pinMode(LED, b.OUTPUT);
+b.pinMode(AL, b.INPUT);
+
 setInterval(checkPIR, 2500); // Checks the Sensor Every 2.5 Seconds
 
 function checkPIR(){
-b.digitalRead('P8_19', printStatus);
+    b.digitalRead('P8_19', printStatus);
 }
 
 function printStatus(err, value) {
-    if(value === 0){
-         b.digitalWrite(led, 1);
-    console.log("Motion Detected");
+    if(value === 0) {
+        b.digitalWrite(LED, 1);
+        console.log("Motion Detected");
     }
     else{
-    console.log("No Motion Detected");
-         b.digitalWrite(led, 0);
+        console.log("No Motion Detected");
+        b.digitalWrite(LED, 0);
     }
 }
