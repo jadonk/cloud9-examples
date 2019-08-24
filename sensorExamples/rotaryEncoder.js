afd04203bcb328451556822ab5a906bc046bacdc
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-08-24 14:12:49 -0400

    Got working on AI

--- sensorExamples/rotaryEncoder.js
+++ sensorExamples/rotaryEncoder.js
@@ -1,23 +1,32 @@
 #!/usr/bin/env node
 // This uses the eQEP hardware to read a rotary encoder
 
-var fs = require('fs');
+const fs = require('fs');
+
+// Black  
+const eQEP0 = "/sys/devices/platform/ocp/48300000.epwmss/48300180.eqep/",
+	  eQEP1 = "/sys/devices/platform/ocp/48302000.epwmss/48302180.eqep/",
+	  eQEP2 = "/sys/devices/platform/ocp/48304000.epwmss/48304180.eqep/"
+
+// AI
+// eQEP2:  P8.33 and P8.45
+// eQEP3:  P8.11 and P8.12 or P9.27 and P9.42
+const eQEP2AI = "/sys/devices/platform/44000000.ocp/4843e000.epwmss/4843e180.eqep/",
+      eQEPXAI = "/sys/devices/platform/44000000.ocp/48442000.epwmss/48442180.eqep/",
+      eQEP3AI = "/sys/devices/platform/44000000.ocp/48440000.epwmss/48440180.eqep/"
     
-var eQEP0 = "/sys/devices/platform/ocp/48300000.epwmss/48300180.eqep/",
-    eQEP1 = "/sys/devices/platform/ocp/48302000.epwmss/48302180.eqep/",
-    eQEP2 = "/sys/devices/platform/ocp/48304000.epwmss/48304180.eqep/",
-    eQEP = eQEP2;
+eQEP = eQEP2AI;
 	
 var oldData,			// pervious data read
-    period = 100;	// in ms
+    period = 100;		// in ms
 
-// Set pinmux for P8_11 and P8_12
-fs.writeFile("/sys/devices/platform/ocp/ocp:P8_11_pinmux/state", "qep", function(err) {
-	if (err) throw err;
-});
-fs.writeFile("/sys/devices/platform/ocp/ocp:P8_12_pinmux/state", "qep", function(err) {
-	if (err) throw err;
-});
+// Set pinmux for P8_11 and P8_12 on Black
+// fs.writeFile("/sys/devices/platform/ocp/ocp:P8_11_pinmux/state", "qep", function(err) {
+// 	if (err) console.log("writeFile err: ", err);
+// });
+// fs.writeFile("/sys/devices/platform/ocp/ocp:P8_12_pinmux/state", "qep", function(err) {
+// 	if (err) console.log("writeFile err: ", err);
+// });
 
 // Set the eEQP period, convert to ns.
 fs.writeFile(eQEP+'period', period*1000000, function(err) {
