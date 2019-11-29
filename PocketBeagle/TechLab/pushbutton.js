950e02c0d0be04c355e763cbbdb320349f3aad08
Jason Kridner <jdk@ti.com>
2019-11-29 20:49:59 +0000

    pushbutton.js: remove bonescript

--- PocketBeagle/TechLab/pushbutton.js
+++ PocketBeagle/TechLab/pushbutton.js
@@ -1,26 +1,28 @@
 #!/usr/bin/env node
-var b = require('bonescript');
-var button = "P2_33";
+// Pre-steps (may need to eventually put in this program):
+//   * Make sure /sys/class/gpio/gpio45 is exported
+//   * Make sure pinmux is in gpio mode
+//   * Make sure gpio is in input mode
+var Epoll = require('epoll').Epoll;
+var fs = require('fs');
+var button = fs.openSync("/sys/class/gpio/gpio45/value", "r");  // P2_33
+var buffer = Buffer.alloc(1);
 
-console.log('Hit ^C to stop');
-b.pinMode(button, b.INPUT, 7, null, null, doAttach);
+fs.writeFileSync('/sys/class/gpio/gpio45/edge', "both");
+doRead(button);
+var poller = new Epoll(printStatus);
+poller.add(button, Epoll.EPOLLPRI);
+console.log("Ready. Please press 'L' button on TechLab. Hit ^C to stop.");
 
-function doAttach(err, x) {
-  if(err) {
-    console.log('pinMode err = ' + err);
-    return;
-  }
-  b.attachInterrupt(button, true, b.CHANGE, printStatus);
+function doRead(fd) {
+  fs.readSync(fd, buffer, 0, 1, 0);
+  process.stdout.write('value = ' + buffer.toString() + '          \r');
 }
 
-function printStatus(err, x) {
+function printStatus(err, fd, events) {
   if(err) {
-    console.log('attachInterrupt err = ' + err);
+    console.log('err = ' + err);
     return;
   }
-  if(x.attached) {
-    console.log("Ready. Please press 'L' button on TechLab.");
-    return;
-  }
-  process.stdout.write('value = ' + x.value + '          \r');
+  doRead(fd);
 }
