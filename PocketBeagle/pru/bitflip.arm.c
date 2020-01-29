585cc6888b9a83d942f53e6b65111ccc2d772e7e
Jason Kridner <jdk@ti.com>
2020-01-29 16:03:09 +0000

    PocketBeagle/pru/bitflip.arm.c: work with dt patch from zmatt

--- PocketBeagle/pru/bitflip.arm.c
+++ PocketBeagle/pru/bitflip.arm.c
@@ -18,36 +18,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <errno.h>
 
 #define PRUSS_SHARED_RAM_OFFSET		0x10000
 
 int main(int argc, const char** argv) {
 	unsigned int i, j, mem_dev;
-	off_t offset = 0;
+	off_t alloc_offset = 0x2000;		/* offset must be 0x2000 for this driver */
+	size_t size = 16; 					/* grab 16 bytes of shared dataram */
 	char * driver = "/dev/uio0";
+	int access_offset = 0;
 
 	/* Open shared memory driver */
 	if(argc>1)
 	    driver = strdup(argv[1]);
 
-	/* Use specified offset */
+	/* Use specified allocaiton offset */
 	if(argc>2)
-		offset = (off_t)strtol(argv[2],0,0);
+		alloc_offset = (off_t)strtol(argv[2],0,0);
+
+	/* Use specified access offset */
+	if(argc>3) {
+		access_offset = (off_t)strtol(argv[3],0,0);
+	}
+
+	/* Use specified size */
+	if(argc>4) {
+		size = (off_t)strtol(argv[4],0,0);
+	}
 
 	/* Allocate shared memory pointer to PRU0 DATARAM */
-	printf("Opening %s offset 0x%08x\n", driver, offset);
+	size += access_offset; /* In case allocation must have a different offset */
+	printf("Opening %s offset 0x%08x and size 0x%08x with access offset 0x%08x\n", 
+		driver, alloc_offset, size, access_offset);
 	mem_dev = open(driver, O_RDWR | O_SYNC);
 	volatile int *shared_dataram = mmap(NULL,
-		16+PRUSS_SHARED_RAM_OFFSET,	/* grab 16 bytes of shared dataram, must allocate with offset of 0 */
+		size,
 		PROT_READ | PROT_WRITE,
 		MAP_SHARED,
 		mem_dev,
-		offset		/* offset must be 0 for /dev/uio0 */
+		alloc_offset
 	);
-	if(!shared_dataram) {
-		
+	if(shared_dataram == MAP_FAILED) {
+		perror("mmap");
+		exit(-1);
 	}
-	shared_dataram += (PRUSS_SHARED_RAM_OFFSET/4);
+	shared_dataram += (access_offset/4);
 
 	printf("shared_dataram = %p\n", shared_dataram);
 	j = *shared_dataram;
