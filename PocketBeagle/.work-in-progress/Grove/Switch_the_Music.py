b8f57861d5ea898d798a56902448c9da5d4acc5e
HansonCc <595355940@qq.com>
2020-02-07 15:28:23 +0800

    added timer to display LCD

--- PocketBeagle/.work-in-progress/Grove/Switch_the_Music.py
+++ PocketBeagle/.work-in-progress/Grove/Switch_the_Music.py
@@ -7,9 +7,18 @@ import wave
 import pyaudio
 import os 
 import time
+import threading
 from Button import BUTTON
 from LCD import JHD1802
 from Shell import GetCmdReturn
+def FunTimer():
+    """Update the LCD every 0.25 seconds"""
+    global MusciIndex
+    global files
+    Lcd.SetText("scale: \r\n{}".format(files[MusciIndex]))
+    global timer
+    timer = threading.Timer(0.25, FunTimer)
+    timer.start()
 def PlayMusic(file):
     """Play WAV format music when the Button is pressed 
         file:the Wav format music
@@ -46,17 +55,25 @@ def PlayMusic(file):
     # close PyAudio
     p.terminate()
 def main():
-    MusciIndex = 0
     #Check the xxx.wav whether exist
     #Move to /tmp/scale/ if exist 
     if os.path.exists('/var/lib/cloud9/PocketBeagle/Grove/*.wav'): 
         GetCmdReturn('sudo mv /var/lib/cloud9/PocketBeagle/Grove/*.wav \
         /tmp/scale/')
+    global files
     files= os.listdir("/tmp/scale")
     print(files)
+    global MusciIndex
+    MusciIndex = 0
     global keys
     keys = BUTTON()
+    global Lcd
     Lcd = JHD1802()
+    # Create timer thread that weak up after 0.25s 
+    # The timer will execute the FunTimer after 0.25s     
+    global timer
+    timer = threading.Timer(0.25, FunTimer)
+    timer.start() 
     while True:
         KeyStatus = keys.GetKeyStatus()
         #KeyStatus isn't empty when the Button is pressed
@@ -66,15 +83,12 @@ def main():
                 MusciIndex = MusciIndex + 1
                 if MusciIndex > 6:
                     MusciIndex = 0
-                Lcd.SetText("scale: \r\n{}".format(files[MusciIndex]))
-                PlayMusic("/tmp/scale/%s"%files[MusciIndex])
             #The KeyStatus[0] = 257 will play last music and the music namedisplay on LCD 
             if KeyStatus[0] == 257:
                 MusciIndex = MusciIndex - 1
                 if MusciIndex < 0:
                     MusciIndex = 6
-                Lcd.SetText("scale: \r\n{}".format(files[MusciIndex]))
-                PlayMusic("/tmp/scale/%s"%files[MusciIndex])
+            PlayMusic("/tmp/scale/%s"%files[MusciIndex])
         time.sleep(0.05)
 if __name__ == "__main__":
     main()
