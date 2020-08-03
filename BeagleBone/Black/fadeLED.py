398cb8d531bcff0082df2900bc6b777ff082ac8b
Mark A. Yoder <Mark.A.Yoder@beagleboard.org>
2020-08-03 17:15:40 -0400

    Switched to Black pins

--- BeagleBone/Black/fadeLED.py
+++ BeagleBone/Black/fadeLED.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 #//////////////////////////////////////
 # 	fadeLED.py
-#   Fades the LED wired to P1_36 using the PWM.
-# 	Wiring:	P1_36 connects to the plus lead of an LED.  The negative lead of the
+#   Fades the LED wired to P9_14 using the PWM.
+# 	Wiring:	P9_14 connects to the plus lead of an LED.  The negative lead of the
 # 			LED goes to a 220 Ohm resistor.  The other lead of the resistor goes
-# 			to 3.3V (P1_14).
+# 			to 3.3V (P9_3).
 #//////////////////////////////////////
 import Adafruit_BBIO.PWM as PWM
 import time
-LED = 'P1_36'
+LED = 'P9_14'
 step = 10       # Step size
 min =  0        # dimmest value
 max =  100      # brightest value
