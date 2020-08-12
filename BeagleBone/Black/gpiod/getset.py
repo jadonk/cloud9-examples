0fa90c0df72096b432d8a737b5d5b5b1f755981b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-08-12 09:12:57 -0400

    Switched to P8_16 in

--- BeagleBone/Black/gpiod/getset.py
+++ BeagleBone/Black/gpiod/getset.py
@@ -2,8 +2,8 @@
 # //////////////////////////////////////
 # 	getset.py
 #  Get the value of P8_11 and write it to P9_14. 
-#     P8_11 is line 13 on chip 1.  P9_14 is line 18 of chip 1.
-# 	Wiring:	Attach a switch to P8_13 and 3.3V and an LED to P9_14.
+#     P8_16 is line 14 on chip 1.  P9_14 is line 18 of chip 1.
+# 	Wiring:	Attach a switch to P8_16 and 3.3V and an LED to P9_14.
 # 	Setup:	sudo apt uupdate; sudo apt install libgpiod-dev
 # 	See:	https://github.com/starnight/libgpiod-example/blob/master/libgpiod-led/main.c
 # //////////////////////////////////////
@@ -14,7 +14,7 @@ import sys
 
 CONSUMER='getset'
 CHIP='1'
-getoffsets=[13] # P8_11
+getoffsets=[14] # P8_16
 setoffests=[18] # P9_14
 
 chip = gpiod.Chip(CHIP)
@@ -30,8 +30,8 @@ print("Hit ^C to stop")
 while True:
     vals = getlines.get_values()
     
-    for val in vals:
-        print(val, end=' ')
-    print('\r', end='')
+    # for val in vals:
+    #     print(val, end=' ')
+    # print('\r', end='')
 
     setlines.set_values(vals)
