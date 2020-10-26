7f9d6e9a671f6a84cf966160a17f2180943e185f
Deepak Khatri <deepaklorkhatri7@gmail.com>
2020-10-26 15:18:34 +0530

    add & update macros for BBB and BBAI PRU pins
    New BBB and BBAI macros has been added. Updated comments and older BBB macros.

--- common/prugpio.h
+++ common/prugpio.h
@@ -1,4 +1,6 @@
 // This file defines the GPIO port addresses and PRU address
+// See the cape interface spec page for more info on PRU 
+// https://elinux.org/Beagleboard:BeagleBone_cape_interface_spec#PRU
 
 #if defined(CHIP) && defined(CHIP_IS_am57xx)
 #warning "Found AI"
@@ -21,25 +23,73 @@
 #define USR4 (1<< 7)
 // GPIO5
 #define USR1 (1<< 5)
-// The define a couple of GPIO pin addresses
-// GPIO6
-#define P9_25   (1<<17)
-// GPIO8
-#define P8_17   (1<18)
-
-// These define which bin in register R30 go to which header pins
-// R30 bits - Output on pru1_1
-#define P8_15	(1<<16)
-#define P8_16	(1<<18)
-#define P8_26	(1<<17)
-// #define P9_14	(1<<14)     // Not defaulted to PRU
-#define P9_16	(1<<15)
-// R31 bits - Input on pru1_1
-#define P8_13	(1<<7)
-#define P8_18	(1<<5)
-#define P8_19	(1<<6)
-#define P9_19	(1<<2)
-#define P9_20	(1<<1)
+// PRU header pins
+// pru1_0
+#define P8_11   (1<<4)		/* pr1_pru0 */
+#define P8_12   (1<<3)		/* pr1_pru0 */
+#define P9_15   (1<<5)		/* pr1_pru0 */
+#define P9_26   (1<<17)		/* pr1_pru0 */
+// pru1_1
+#define P8_13   (1<<7)		/* pr1_pru1 */
+#define P8_14   (1<<9)		/* pr1_pru1 */
+#define P8_15   (1<<16)		/* pr1_pru1 */
+#define P8_16   (1<<18)		/* pr1_pru1 */
+#define P8_18   (1<<5)		/* pr1_pru1 */
+#define P8_19   (1<<6)		/* pr1_pru1 */
+#define P8_26   (1<<17)		/* pr1_pru1 */
+#define P9_14   (1<<14)		/* pr1_pru1 */
+#define P9_16   (1<<15)		/* pr1_pru1 */
+#define P9_19   (1<<2)		/* pr1_pru1 */
+#define P9_20   (1<<1)		/* pr1_pru1 */
+#define P9_27   (1<<11)		/* pr1_pru1 */
+#define P9_41   (1<<3)		/* pr1_pru1 */
+#define P9_42   (1<<10)		/* pr1_pru1 */
+// pru2_0
+#define P8_03   (1<<10)		/* pr2_pru0 */
+#define P8_04   (1<<11)		/* pr2_pru0 */
+#define P8_05   (1<<6)		/* pr2_pru0 */
+#define P8_06   (1<<7)		/* pr2_pru0 */
+#define P8_08   (1<<20)		/* pr2_pru0 */
+#define P8_17   (1<<15)		/* pr2_pru0 */
+#define P8_20   (1<<3)		/* pr2_pru0 */
+#define P8_21   (1<<2)		/* pr2_pru0 */
+#define P8_22   (1<<9)		/* pr2_pru0 */
+#define P8_23   (1<<8)		/* pr2_pru0 */
+#define P8_24   (1<<5)		/* pr2_pru0 */
+#define P8_25   (1<<4)		/* pr2_pru0 */
+#define P8_28   (1<<17)		/* pr2_pru0 */
+#define P8_29   (1<<18)		/* pr2_pru0 */
+#define P8_30   (1<<19)		/* pr2_pru0 */
+#define P8_31   (1<<11)		/* pr2_pru0 */
+#define P8_33   (1<<10)		/* pr2_pru0 */
+#define P8_34   (1<<8)		/* pr2_pru0 */
+#define P8_35   (1<<9)		/* pr2_pru0 */
+#define P8_36   (1<<7)		/* pr2_pru0 */
+#define P8_37   (1<<5)		/* pr2_pru0 */
+#define P8_38   (1<<6)		/* pr2_pru0 */
+#define P8_39   (1<<3)		/* pr2_pru0 */
+#define P8_40   (1<<4)		/* pr2_pru0 */
+#define P8_41   (1<<1)		/* pr2_pru0 */
+#define P8_42   (1<<2)		/* pr2_pru0 */
+#define P8_44   (1<<0)		/* pr2_pru0 */
+#define P9_11   (1<<14)		/* pr2_pru0 */
+#define P9_13   (1<<15)		/* pr2_pru0 */
+// pru2_1
+#define P8_07   (1<<16)		/* pr2_pru1 */
+#define P8_09   (1<<6)		/* pr2_pru1 */
+#define P8_10   (1<<15)		/* pr2_pru1 */
+#define P8_27   (1<<17)		/* pr2_pru1 */
+#define P8_32   (1<<0)		/* pr2_pru1 */
+#define P8_43   (1<<20)		/* pr2_pru1 */
+#define P8_45   (1<<18)		/* pr2_pru1 */
+#define P8_46   (1<<19)		/* pr2_pru1 */
+#define P9_17   (1<<9)		/* pr2_pru1 */
+#define P9_18   (1<<8)		/* pr2_pru1 */
+#define P9_25   (1<<5)		/* pr2_pru1 */
+#define P9_28   (1<<13)		/* pr2_pru1 */
+#define P9_29   (1<<11)		/* pr2_pru1 */
+#define P9_30   (1<<12)		/* pr2_pru1 */
+#define P9_31   (1<<10)		/* pr2_pru1 */
 
 // Shared memory
 #define AM33XX_DATARAM0_PHYS_BASE1		0x4b200000
@@ -54,7 +104,7 @@
 
 #warning "Found am335x"
 // These are addresses for the am35xx
-#define GPIO0 0x44E07000        // From table 2.2 of am335x TRM
+#define GPIO0 0x44E07000		  // From table 2.2 of am335x TRM
 #define GPIO1 0x4804C000
 #define GPIO2 0x481AC000
 #define GPIO3 0x481AE000
@@ -77,7 +127,7 @@
 #define P2_1 (1<<18)
 #define P1_32 (1<<10)
 
-// R30 output bits on pru0
+// R31/R30 input/output bits on pru0
 #define P9_31   (1<<0)
 #define P9_29   (1<<1)
 #define P9_30   (1<<2)
@@ -86,6 +136,16 @@
 #define P9_27   (1<<5)
 #define P9_91   (1<<6)
 #define P9_25   (1<<7)
+// R30 output bits on pru0
+#define P8_11   (1<<15) /* output */
+#define P8_12   (1<<14) /* output */
+// R31 input bits on pru0
+#define P8_15   (1<<15) /* input */
+#define P8_16   (1<<14) /* input */
+#define P9_24   (1<<16) /* input */
+
+// R31 input bits on pru1
+#define P9_26   (1<<16) /* input */
 
 // R30 output bits on pru0 on Pocket
 #define P1_36   (1<<0)
@@ -104,6 +164,6 @@
 
 #endif
 // /4 to convert from byte address to word address
-#define GPIO_CLEARDATAOUT	0x190/4     // Write 1 here to set a given bit    
-#define GPIO_SETDATAOUT 	0x194/4     // A 1 here clears the corresponding bit
-#define GPIO_DATAOUT		0x138/4     // For reading the GPIO registers
+#define GPIO_CLEARDATAOUT	0x190/4		// Write 1 here to set a given bit    
+#define GPIO_SETDATAOUT 	0x194/4		// A 1 here clears the corresponding bit
+#define GPIO_DATAOUT		0x138/4		// For reading the GPIO registers
